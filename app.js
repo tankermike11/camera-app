9d6c1a693894b2e1e221fbfa7a91cf711c2cb718
tankermike11 <mabkakeroyd@gmail.com>
2019-02-16 21:46:18 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const cameraView = document.querySelector("#camera--view"),
     cameraTrigger = document.querySelector("#camera--trigger")
 // Access the device camera and stream to cameraView
 function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
+navigator.mediaDevices.getUserMedia({
+  video: {
+    facingMode: {
+      exact: 'environment'
+      }
+    }
+  })
     .catch(function(error) {
         console.error("Oops. Something is broken.", error);
     });
